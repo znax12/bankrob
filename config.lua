f660e62df9d57b69832cf616d33c3083b52d44ea
znax12 <58237276+znax12@users.noreply.github.com>
2020-02-05 02:31:20 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.Marker = {
+	r = 250, g = 0, b = 0, a = 100,  -- red color
+	x = 1.0, y = 1.0, z = 1.5,       -- tiny, cylinder formed circle
+	DrawDistance = 15.0, Type = 1    -- default circle type, low draw distance due to indoors area
+}
+
+Config.PoliceNumberRequired = 2
+Config.TimerBeforeNewRob    = 1800 -- The cooldown timer on a store after robbery was completed / canceled, in seconds
+
+Config.MaxDistance    = 100   -- max distance from the robbary, going any longer away from it will to cancel the robbary
+Config.GiveBlackMoney = false -- give black money? If disabled it will give cash instead
+
+Stores = {
+	["pacific_bank"] = {
+		position = { x = 254.04, y = 225.3, z = 101.88 },
+		reward = math.random(5000, 35000),
+		nameOfStore = "Pacific Standard Public Deposit Bank",
+		secondsRemaining = 350, -- seconds
+		lastRobbed = 0
+	}
+}
